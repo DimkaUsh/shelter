1fc917c4ae77eaa1fd9177e10c3c6e5fd81df05e
Dmitrii Ushakov <81690819+DimkaUsh@users.noreply.github.com>
2022-06-08 20:16:45 +0800

    feat: add shelter project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import pagination from "./modules/pagination.js";
+import slider from "./modules/slider.js";
+
+window.addEventListener("DOMContentLoaded", function () {
+  slider();
+  pagination();
+});
